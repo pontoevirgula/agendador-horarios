98da363b68e25fd57c4a7993721bc1b07840972a
Carlos Lima <inv.clima@ifood3rd.com>
2026-03-22 19:16:04 -0300

    Initial commit

---
+++ src/main/kotlin/com/chslcompany/agendador_horarios/AgendadorHorariosApplication.kt
@@ -0,0 +1,11 @@
+package com.chslcompany.agendador_horarios
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AgendadorHorariosApplication
+
+fun main(args: Array<String>) {
+	runApplication<AgendadorHorariosApplication>(*args)
+}
---
+++ src/test/kotlin/com/chslcompany/agendador_horarios/AgendadorHorariosApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.chslcompany.agendador_horarios
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AgendadorHorariosApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
